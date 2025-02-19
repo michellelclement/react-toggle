261d48d5ff36e16af5d00645b043a89f10943ecc
Michelle Clement <65297933+michellelclement@users.noreply.github.com>
2025-02-19 13:32:32 +0000

    Style single slider

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -7,14 +7,14 @@ const Toggle = () => {
         type="radio"
         className="toggle-option toggle-start"
         id="option1"
-        name="toggle-option2"
+        name="toggle-option"
       />
       <label htmlFor="option1">Option 1</label>
       <input
         type="radio"
         className="toggle-option toggle-end"
         id="option2"
-        name="toggle-option2"
+        name="toggle-option"
       />
       <label htmlFor="option2">Option 2</label>
       <div className="toggle-option-slider"></div>
