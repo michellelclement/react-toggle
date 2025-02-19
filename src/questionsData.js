b65a590ff4ada53aa5e7830b648af34a07b0a20c
Michelle Clement <65297933+michellelclement@users.noreply.github.com>
2025-02-19 14:04:03 +0000

    Add test question data:

---
+++ src/questionsData.js
@@ -0,0 +1,26 @@
+const questionsData = [
+  {
+    id: 1,
+    options: [
+      { id: "option1", text: "Cell Wall" },
+      { id: "option2", text: "Ribosomes" },
+    ],
+    correctAnswer: "option1",
+  },
+  {
+    id: 2,
+    options: [
+      { id: "option1", text: "Cytoplasm" },
+      { id: "option2", text: "Chloroplast" },
+    ],
+    correctAnswer: "option2",
+  },
+  {
+    id: 3,
+    options: [
+      { id: "option1", text: "Partially permeable membrane" },
+      { id: "option2", text: "Impermeable membrane" },
+    ],
+    correctAnswer: "option1",
+  },
+];
