dedaaef6e9ff167f40b62424344dfee5ac9bbb0e
Michelle Clement <65297933+michellelclement@users.noreply.github.com>
2025-02-19 14:03:24 +0000

    Add state to check which option is selected and if it matches correct option

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Toggle = () => {
+  const [selectedOption, setSelectedOption] = useState(null);
+  const correctOption = "option2"; //Set correct option
+
+  const clicked = (e) => {
+    setSelectedOption(e.target.id);
+  };
+
   return (
     <div className="toggle">
       <input
@@ -8,6 +15,7 @@ const Toggle = () => {
         className="toggle-option toggle-start"
         id="option1"
         name="toggle-option"
+        onChange={clicked}
       />
       <label htmlFor="option1">Option 1</label>
       <input
@@ -15,9 +23,16 @@ const Toggle = () => {
         className="toggle-option toggle-end"
         id="option2"
         name="toggle-option"
+        onChange={clicked}
       />
       <label htmlFor="option2">Option 2</label>
       <div className="toggle-option-slider"></div>
+
+      <p>
+        {selectedOption === correctOption
+          ? "Correct Answer"
+          : "Incorrect sorry!"}
+      </p>
     </div>
   );
 };
